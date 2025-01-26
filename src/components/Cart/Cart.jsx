a15c9242c73506f24f939dadcb09cf5c26e05b7d
lutalivre <fsakmexico@gmail.com>
2025-01-25 21:21:56 -0600

    corrección a imagen de carrito, botón de eleiminar y eliminar todo

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,26 +1,37 @@
 import { useContext } from "react"
-import { CartContext } from "../../context/CartContex"
+import { CartContext } from "../../context/CartContex.jsx"
 
 const Cart = () => {
-    const { cart, totalPrice } = useContext(CartContext)
+    const { cart, totalPrice, deleteProductById, deleteCart } = useContext(CartContext)
+
   return (
     <div>
-      <ul>
+      <h1>Productos en el Carrito</h1>
         {
+          cart.length > 0 ? (
             cart.map((productCart)=> (
-            <li key={productCart.id} style={ { display: "flex", justifyContent: "space-around", margin: "20px 0"}}>
-                <img src={productCart.image} width={100} />
+            <div key={productCart.id} style={ { display: "flex", justifyContent: "space-around", margin: "20px 0"}}>
+               <img
+            src={productCart.image[0] || "/img/placeholder.png"} // Si no hay imágenes, usa placeholder.png
+            alt={productCart.name}
+            style={{ width: "100px", height: "auto" }}
+            onError={(e) => (e.target.src = "/img/placeholder.png")} // Imagen de respaldo
+          />
                 <p>{productCart.name}</p>
                 <p> Precio de c/u ${productCart.price} MXN</p>
                 <p>cantidad: {productCart.quantity}</p>
-            </li>
-            
+                <p>Precio Parcial: ${productCart.price * productCart.quantity}</p>
+                <button onClick={ ()=> deleteProductById(productCart.id) }>Eliminar</button>
+            </div>
             ))
-            
+          ): (
+            <p>No hay Productos en el Carrito.</p>
+          )
         }
-      </ul>
-            <h3>Precio total:$ {totalPrice()} MXN </h3>
-
+      <div>
+            <p>Precio total:${totalPrice()} MXN </p>
+            <button onClick={deleteCart} >Vaciar Carrito</button>
+      </div>
     </div>
   )
 }
